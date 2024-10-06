75690c08bf5ebce317258643f65b196057031730
Shruti Merchant <shruti.merchant@gmail.com>
2024-10-06 01:43:06 -0700

    Built output page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Output from './pages/Output';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/output" element={<Output />} />
         {/* You can add more routes here for additional pages */}
       </Routes>
     </Router>
